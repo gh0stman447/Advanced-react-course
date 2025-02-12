af8e901021663e9eaec29b4ea38484baf9ec25d9
MalyginOleg <Omalygin@korusconsulting.ru>
2025-02-12 21:34:09 +0000

    Добавил роутер и саспенс для асинхронных чанков

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Counter } from './components/Counter';
+import { Route, Routes, Link } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { Suspense } from 'react';
 
 export const App = () => {
   return (
     <div className='app'>
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О нас</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<MainPageAsync />} />
+          <Route path='/about' element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,5 @@
+export const MainPage = () => {
+  return <div>MainPage13 213123 123 1 1</div>;
+};
+
+export default MainPage;
