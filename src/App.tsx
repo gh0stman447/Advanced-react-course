46f64a944943aed7a0280f15baa258320f4b7942
MalyginOleg <Omalygin@korusconsulting.ru>
2025-02-12 22:37:26 +0000

    Настроил темизацию

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { Route, Routes, Link } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { Suspense } from 'react';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О нас</Link>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path='/' element={<MainPageAsync />} />
