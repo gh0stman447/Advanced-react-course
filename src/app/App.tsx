a040a0ce726e09669828f0c4bdfcaeae26523db0
MalyginOleg <Omalygin@korusconsulting.ru>
2025-02-13 13:45:17 +0000

    Вынес роутинг, настроил темизацию, вынес header в виджет

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,18 @@
-import { Route, Routes, Link } from 'react-router-dom';
-import { Suspense } from 'react';
-import './styles/index.scss';
 import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { MainPageAsync } from 'pages/MainPage/ui/MainPage.async';
-import { AboutPageAsync } from 'pages/AboutPage/ui/AboutPage.async';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'wigets/Navbar';
+import './styles/index.scss';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О нас</Link>
+      <Navbar />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<MainPageAsync />} />
-          <Route path='/about' element={<AboutPageAsync />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
+
